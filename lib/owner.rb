4b390a1cd4d62f39a61d2124433bfcdc57ce344f
github username <github email address>
2018-03-01 12:00:31 -0500

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,59 @@
 class Owner
   # code goes here
+  attr_accessor :pets
+  attr_reader :species, :name
+  @@all=[]
+  def initialize(species)
+    @species=species
+    @@all << self
+    @pets={:fishes => [], :dogs => [], :cats => []}
+  end
+  def self.all
+    @@all
+  end
+  def self.reset_all
+    @@all=[]
+  end
+  def self.count
+    @@all.size
+  end
+  def say_species
+    "I am a #{@species}."
+  end
+  def name=(name)
+    @name=name
+  end
+  def buy_fish(name)
+    fish = Fish.new(name)
+    @pets[:fishes] << fish
+  end
+  def buy_cat(name)
+    cat = Cat.new(name)
+    @pets[:cats] << cat
+  end
+  def buy_dog(name)
+    dog = Dog.new(name)
+    @pets[:dogs] << dog
+  end
+  def walk_dogs
+    @pets[:dogs].map {|dog| dog.mood='happy'}
+  end
+  def play_with_cats
+    @pets[:cats].map {|cat| cat.mood='happy'}
+  end
+  def feed_fish
+    @pets[:fishes].map {|fish| fish.mood='happy'}
+  end
+  def sell_pets
+    @pets[:dogs].map {|dog| dog.mood='nervous'}
+    @pets[:cats].map {|cat| cat.mood='nervous'}
+    @pets[:fishes].map {|fish| fish.mood='nervous'}
+    @pets={:fishes => [], :dogs => [], :cats => []}
+  end
+  def list_pets
+    dogsize = pets[:dogs].size
+    catsize = pets[:cats].size
+    fishsize = pets[:fishes].size
+    "I have #{fishsize} fish, #{dogsize} dog(s), and #{catsize} cat(s)."
+  end
 end
